f37bee03c042aa6b6660854f77f844fa1c381bfc
Syed Shoebuddin <ushoeb25@gmail.com>
2025-10-18 15:05:53 +0530

    feat: add lucide-react and vite-plugin-pwa dependencies; configure Vite to use PWA plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,13 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [
+    tailwindcss(),
+    reactRouter(),
+    tsconfigPaths(),
+    VitePWA({ registerType: "autoUpdate" }),
+  ],
 });
